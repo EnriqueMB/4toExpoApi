7fb08685c03bcec29382b8e6f2825ffa75f63aa4
DavidRVF <143049524+DavidRVF@users.noreply.github.com>
2024-05-01 15:32:38 -0600

    Create Servicios.cs

---
+++ 4toExpoApi.DataAccess/Entities/Servicios.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace _4toExpoApi.DataAccess.Entities
+{
+    public class Servicios
+    {
+        public int Id { get; set; }
+        public string Servicio { get; set; }
+        public string Descripcion { get; set; }
+        public string DiasLaborales {  get; set; }
+        public DateTime? HoraInicio { get; set; }
+        public DateTime HoraFinal {  get; set; }
+
+
+    }
+}
