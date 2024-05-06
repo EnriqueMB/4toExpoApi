df1adf7162957dfeabfafc45a6530acfd3d564a4
Froilan-Suarez23 <froilan.surozz.6a97@gmail.com>
2024-05-06 17:45:13 -0600

    Editar Conatador

--- 4toExpoApi_v1.0.0/Controllers/ContadorController.cs
+++ 4toExpoApi_v1.0.0/Controllers/ContadorController.cs
@@ -1,4 +1,5 @@
-﻿using _4toExpoApi.Core.Services;
+﻿using _4toExpoApi.Core.Request;
+using _4toExpoApi.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
@@ -30,6 +31,34 @@ namespace _4toExpoApi_v1._0._0.Controllers
                 _logger.LogInformation(MethodBase.GetCurrentMethod().DeclaringType.DeclaringType.Name + "Finished Success");
 
                 return Ok (contador);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(MethodBase.GetCurrentMethod().DeclaringType.DeclaringType.Name + ex.Message);
+                throw;
+            }
+        }
+
+        [HttpPut("EditarContador")]
+        public async Task<IActionResult> EditarContador(ContadorRequets request)
+        {
+            try
+            {
+                _logger.LogInformation(MethodBase.GetCurrentMethod().DeclaringType.DeclaringType.Name + "Started Success");
+
+
+                var response = await _contadorService.EditarContador(request, 1);
+
+                if (response.Success)
+                {
+                    _logger.LogInformation(MethodBase.GetCurrentMethod().DeclaringType.DeclaringType.Name + "Finished Success");
+
+                    return Ok(response);
+                }
+
+                _logger.LogInformation(MethodBase.GetCurrentMethod().DeclaringType.DeclaringType.Name + "Finished Success");
+
+                return BadRequest(response);
             }
             catch (Exception ex)
             {
