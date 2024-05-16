b93b511c2823b3635f082fbc149150e39e135ef5
EnocVZ <enoch.95@hotmail.com>
2024-05-16 12:16:11 -0600

    api para hoteles

--- 4toExpoApi.DataAccess/IRepositories/IHotelRepository.cs
+++ 4toExpoApi.DataAccess/IRepositories/IHotelRepository.cs
@@ -13,7 +13,9 @@ namespace _4toExpoApi.DataAccess.IRepositories
     public interface IHotelRepository : IBaseRepository<Hotel>
     {
         Task<GenericResponse<Hotel>> AgregarHotel(Hotel hotel, List<Habitacion> listHabitacion, List<Distancia>listaDistancia, int userAlt, ILogger logger);
-
         Task<GenericResponse<List<HotelResponse>>> ListaHotel(ILogger logger);
+        Task<GenericResponse<HotelResponse>> HotelPorID(int id, ILogger logger);
+        Task<GenericResponse<Hotel>> ActualizarHotel(Hotel hotel, List<Habitacion> listHabitacion, List<Distancia> listaDistancia, int userAlt, ILogger logger);
+
     }
 }
